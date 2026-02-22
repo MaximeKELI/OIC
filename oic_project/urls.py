a93494d97ffa59fe6f73d8ea2f24d7d52f3a4c6a
MaximeKELI <presidentnetero01@gmail.com>
2026-02-22 16:08:37 +0000

    OIC

--- oic_project/urls.py
+++ oic_project/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
